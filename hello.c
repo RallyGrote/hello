58425159d4b6ff3905bf4feaab6121673f6eb8fa
Rally Grotelueschen <rallygrote@gmail.com>
2022-08-25 20:11:55 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Rally Grotelueschen and Austin M
+ * 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Rally Grotelueschen\n");
+
+  return 0;
+}
